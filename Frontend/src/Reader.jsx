dc9d3af43aad2154aaeb012ddb58c6c2d29758bd
Jean-Marc <jmmichiels1981@gmail.com>
2025-12-17 10:59:02 +0100

    Avant test lecteur

---
+++ Frontend/src/Reader.jsx
@@ -0,0 +1,13 @@
+import "./reader.css";
+
+export default function Reader() {
+    return (
+        <div className="reader">
+            <h1>Chapitre 1</h1>
+
+            <p>Le soleil se levait à peine sur la ville.</p>
+            <p>— Tu es en retard, dit-elle.</p>
+            <p>Il ne répondit pas.</p>
+        </div>
+    );
+}
